3e31d3b73e470b7f09bfbcb53f4ea51c2dee2aa4
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-06-20 22:33:59 +0700

    Add files via upload

---
+++ construction.lua
@@ -0,0 +1,129 @@
+construction = construction or {}
+construction.Data = construction.Data or {}
+
+function construction.Register(name, tbl)
+    construction.Data[name] = tbl
+end
+
+function c_Child(pnl, n)
+    if isstring(n) then
+        for _, pan in pairs(pnl:GetChildren()) do
+            if pan.Name == n then
+                return pan
+            end
+        end
+    end
+    return pnl.children[n]
+end
+
+function construction.Create(tbl, pan)
+    tbl = construction.Data[tbl] or tbl
+    local elm
+	for _, opt in pairs(tbl) do
+        elm = pan and vgui.Create(opt.VGUI, pan) or vgui.Create(opt.VGUI)
+        if opt.Name then    
+            elm.Name = opt.Name
+        end
+
+        if opt.VGUI == "DFrame" then
+            elm:SetDraggable(false)
+            elm:SetMouseInputEnabled(false)
+            elm:SetKeyBoardInputEnabled(false)
+        end
+
+        if opt.Title then
+            elm:SetTitle(opt.Title)
+        end
+
+        if opt.Dock then
+            elm:Dock(opt.Dock)
+        end
+
+		if opt.DockMargin then
+			elm:DockMargin(unpack(opt.DockMargin))
+        end
+        
+        if opt.DockPadding then
+			elm:DockPadding(unpack(opt.DockPadding))
+		end
+
+		if opt.Wide then
+			elm:SetWide(opt.Wide)
+        end
+        
+		if opt.Tall then
+			elm:SetTall(opt.Tall)
+        end
+
+        if opt.Size then
+			elm:SetSize(unpack(opt.Size))
+		end
+        
+        if opt.Center then
+            elm:Center()
+        end
+
+        if opt.Alignment then
+            elm:SetContentAlignment(opt.Alignment)
+        end
+
+        if opt.MakePopup then
+            elm:MakePopup()
+        end
+
+        if opt.SetMouseInputEnabled then
+            elm:SetMouseInputEnabled(opt.SetMouseInputEnabled)
+        end
+
+        if opt.SetKeyBoardInputEnabled then
+            elm:SetKeyBoardInputEnabled(opt.SetKeyBoardInputEnabled)
+        end
+
+        if opt.SetDraggable then
+            elm:SetDraggable(opt.SetDraggable)
+        end
+
+        if opt.ShowCloseButton then
+            elm:ShowCloseButton(opt.ShowCloseButton)
+        end
+
+        if opt.SetSizable then
+            elm:SetSizable(opt.SetSizable)
+        end
+
+		if opt.Text then
+			elm:SetText(opt.Text)
+		end
+
+        if opt.Font then
+            elm:SetFont(opt.Font)
+        end
+
+        if opt.Color then
+            elm:SetColor(opt.Color)
+        end
+
+        if opt.OnRemove then
+            elm.OnRemove = opt.OnRemove
+        end
+
+        if opt.OnClose then
+            elm.OnClose = opt.OnClose
+        end
+
+		if opt.Paint then
+			elm.Paint = opt.Paint
+		end
+
+		if opt.DoClick then
+			elm.DoClick = opt.DoClick
+        end
+
+        if opt.Children then
+            elm.children = elm.children or {}
+            local child = construction.Create(opt.Children, elm)
+            elm.children[#elm.children + 1] = child
+		end
+    end
+    return elm
+end
---
+++ preview.lua
@@ -0,0 +1,72 @@
+--[[
+    Preview 1
+]]
+
+construction.Register("preview_1", {
+    {
+        VGUI = "DFrame",
+        Title = "Construction preview #1",
+        SetDraggable = true, -- now its not draggable by the default
+        Size = {300, 300},
+        Center = true,
+        MakePopup = true,
+        Children = {
+            {
+                VGUI = "DButton",
+                Text = "That does nothing",
+                Dock = FILL,
+                DoClick = function()
+                    print("ok a bit more of nothing")
+                end
+            }
+        }
+    }
+})
+
+concommand.Add("preview_1", function()
+    construction.Create("preview_1")
+end)
+
+--[[
+    Preview 2
+]]
+
+-- main panel
+construction.Register("preview_2", {
+    {
+        VGUI = "DFrame",
+        Title = "Construction preview #2",
+        Size = {300, 500},
+        Center = true,
+        MakePopup = true,
+        Children = {
+            {
+                Name = "scrollPnl",
+                VGUI = "DScrollPanel",
+                Dock = FILL
+            }
+        }
+    }
+})
+
+-- button
+construction.Register("preview_2_helper", {
+    {
+        VGUI = "DButton",
+        Text = "Preview button",
+        Dock = TOP,
+        DockMargin = {4, 4, 4, 4}
+    }
+})
+
+concommand.Add("preview_2", function()
+    local fr = construction.Create("preview_2")
+    for i = 1, 10 do
+        local btn = construction.Create("preview_2_helper", c_Child(fr, "scrollPnl")) 
+        -- we also could use number 1 in c_Child because scroll panel is the first
+
+        if math.random(1, 2) == 2 then
+            btn:SetText("changed")
+        end
+    end
+end)
